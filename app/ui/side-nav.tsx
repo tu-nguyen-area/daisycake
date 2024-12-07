8bda9e5fa1aa94d5062a1cbbba88e4c056931d2f
tunguyenarea <tunguyenarea@gmail.com>
2024-12-07 19:49:24 +0700

    Commit 3

--- app/ui/side-nav.tsx
+++ app/ui/side-nav.tsx
@@ -11,7 +11,7 @@ export default function SideNav() {
       <div className="flex justify-center">
       {links.map((link) => {
         return (
-          <Link className="m-2 md:m-6 w-28 md:w-32 md:h-6 flex justify-center rounded-xl bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white animate-changebutton md:text-base" key={link.name} href={link.href}>
+          <Link className="m-2 md:m-6 w-28 md:w-32 md:h-6 flex justify-center rounded-xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white animate-changebutton" key={link.name} href={link.href}>
             {link.name}
           </Link>
         );
