cbb2701610687abe42c537c5ad41da5fed25a0fd
tunguyenarea <tunguyenarea@gmail.com>
2024-12-28 02:06:12 +0700

    Commit 3

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,17 @@ import SlideShow from '@/app/ui/slide-show';
 import Footer from '@/app/ui/footer';
 
 export default function Page() {
+  const cakeB = [
+    { id: 1, src: "/cake1.png", code: "Cake 001" },
+    { id: 2, src: "/cake2.png", code: "Cake 002" },
+    { id: 3, src: "/cake3.png", code: "Cake 003" }
+  ];
+
+  const cakeL = [
+    { id: 4, src: "/cake4.png", code: "Cake 004" },
+    { id: 5, src: "/cake5.png", code: "Cake 005" },
+    { id: 6, src: "/cake6.png", code: "Cake 006" }
+  ];
 
   return (
     <>
@@ -24,39 +35,21 @@ export default function Page() {
               <p>Best Selling</p>
             </div>
             <div className="flex flex-col md:flex-row justify-around justify-center pb-12 items-center">
-              <div className="">
-                <Image className="rounded-xl w-80 h-64 border-4 border-x-fuchsia-500 border-y-fuchsia-500 hover:md:scale-110 transition-all"
-                  src="/cake1.png"
-                  width={300}
-                  height={0}
-                  alt="cake"
-                />
-                <div className="text-center text-fuchsia-500">
-                  <label>Cake 001</label>
-                </div>
-              </div>
-              <div>
-                <Image className="rounded-xl w-80 h-64 border-4 border-x-fuchsia-500 border-y-fuchsia-500 hover:md:scale-110 transition-all"
-                  src="/cake2.png"
-                  width={300}
-                  height={0}
-                  alt="cake"
-                />
-                <div className="text-center text-fuchsia-500">
-                  <label>Cake 002</label>
-                </div>
-              </div>
-              <div>
-                <Image className="rounded-xl w-80 h-64 border-4 border-x-fuchsia-500 border-y-fuchsia-500 hover:md:scale-110 transition-all"
-                  src="/cake3.png"
-                  width={300}
-                  height={0}
-                  alt="cake"
-                />
-                <div className="text-center text-fuchsia-500">
-                  <label>Cake 003</label>
-                </div>
-              </div>
+              {cakeB.map((cake) => {
+                return (
+                  <div key={cake.id} className="border-4 border-x-fuchsia-500 border-y-fuchsia-500 rounded-xl hover:md:scale-110 transition-all m-2">
+                    <Image className="w-80 h-64 border-2 border-x-fuchsia-500 border-y-fuchsia-500"
+                      src={cake.src}
+                      width={300}
+                      height={2}
+                      alt={cake.code}
+                    />
+                    <div className="text-center text-fuchsia-500">
+                      <label>{cake.code}</label>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </section>
 
@@ -65,80 +58,44 @@ export default function Page() {
               <p>Most Liked</p>
             </div>
             <div className="flex flex-col md:flex-row justify-around justify-center pb-12 items-center">
-              <div className="">
-                <Image className="rounded-xl w-80 h-64 border-4 border-x-fuchsia-500 border-y-fuchsia-500 hover:md:scale-110 transition-all"
-                  src="/cake4.png"
-                  width={300}
-                  height={0}
-                  alt="cake"
-                />
-                <div className="text-center text-fuchsia-500">
-                  <label>Cake 004</label>
-                </div>
-              </div>
-              <div>
-                <Image className="rounded-xl w-80 h-64 border-4 border-x-fuchsia-500 border-y-fuchsia-500 hover:md:scale-110 transition-all"
-                  src="/cake5.png"
-                  width={300}
-                  height={0}
-                  alt="cake"
-                />
-                <div className="text-center text-fuchsia-500">
-                  <label>Cake 005</label>
-                </div>
-              </div>
-              <div>
-                <Image className="rounded-xl w-80 h-64 border-4 border-x-fuchsia-500 border-y-fuchsia-500 hover:md:scale-110 transition-all"
-                  src="/cake6.png"
-                  width={300}
-                  height={0}
-                  alt="cake"
-                />
-                <div className="text-center text-fuchsia-500">
-                  <label>Cake 006</label>
-                </div>
-              </div>
+              {cakeL.map((cake) => {
+                return (
+                  <div key={cake.id} className="border-4 border-x-fuchsia-500 border-y-fuchsia-500 rounded-xl hover:md:scale-110 transition-all m-2">
+                    <Image className="w-80 h-64 border-2 border-x-fuchsia-500 border-y-fuchsia-500"
+                      src={cake.src}
+                      width={300}
+                      height={2}
+                      alt={cake.code}
+                    />
+                    <div className="text-center text-fuchsia-500">
+                      <label>{cake.code}</label>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </section>
 
           <section>
-            <div className=" flex justify-center bg-gradient-to-r from-purple-500 to-pink-500 p-1 text-white text-xl">
+            <div className="flex justify-center bg-gradient-to-r from-purple-500 to-pink-500 p-1 text-white text-xl">
               <p>Recommended</p>
             </div>
             <div className="flex flex-col md:flex-row justify-around justify-center pb-12 items-center">
-              <div>
-                <Image className="rounded-xl w-80 h-64 border-4 border-x-fuchsia-500 border-y-fuchsia-500 hover:md:scale-110 transition-all"
-                  src="/cake1.png"
-                  width={300}
-                  height={0}
-                  alt="cake"
-                />
-                <div className="text-center text-fuchsia-500">
-                  <label>Cake 001</label>
-                </div>
-              </div>
-              <div>
-                <Image className="rounded-xl w-80 h-64 border-4 border-x-fuchsia-500 border-y-fuchsia-500 hover:md:scale-110 transition-all"
-                  src="/cake2.png"
-                  width={300}
-                  height={0}
-                  alt="cake"
-                />
-                <div className="text-center text-fuchsia-500">
-                  <label>Cake 002</label>
-                </div>
-              </div>
-              <div>
-                <Image className="rounded-xl w-80 h-64 border-4 border-x-fuchsia-500 border-y-fuchsia-500 hover:md:scale-110 transition-all"
-                  src="/cake3.png"
-                  width={300}
-                  height={0}
-                  alt="cake"
-                />
-                <div className="text-center text-fuchsia-500">
-                  <label>Cake 003</label>
-                </div>
-              </div>
+              {cakeB.map((cake) => {
+                return (
+                  <div key={cake.code} className="border-4 border-x-fuchsia-500 border-y-fuchsia-500 rounded-xl hover:md:scale-110 transition-all m-2">
+                    <Image className="w-80 h-64 border-2 border-x-fuchsia-500 border-y-fuchsia-500"
+                      src={cake.src}
+                      width={300}
+                      height={2}
+                      alt={cake.code}
+                    />
+                    <div className="text-center text-fuchsia-500">
+                      <label>{cake.code}</label>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </section>
 
@@ -159,10 +116,6 @@ export default function Page() {
           <div className="flex animate-slipslow">
             <SlideLine />
           </div>
-        </section>
-
-        <section>
-          
         </section>
       </div>
 
