e7e79efde6c577212de9a2033c6a707c269645a0
tunguyenarea <tunguyenarea@gmail.com>
2025-01-04 14:30:12 +0700

    Commit 3

--- app/ui/slide-line.tsx
+++ app/ui/slide-line.tsx
@@ -9,7 +9,7 @@ export default function SlideLine() {
   return(
     <>
     
-      <Image className="m-3 rounded-xl w-52 h-48 border-4 rounded-xl border-x-fuchsia-500 border-y-fuchsia-500"
+      <Image className="m-3 rounded-xl w-16 sm:w-52 h-14 sm:h-48 border-2 sm:border-4 rounded-xl border-x-fuchsia-500 border-y-fuchsia-500"
         src={image.url}
         width={200}
         height={0}
