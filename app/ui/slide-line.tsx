a4b089e78b62270ec69bc4eb4781419bc433fa86
tu-nguyen-area <tunguyenarea@gmail.com>
2025-03-27 10:21:44 +0700

    Commit 3

--- app/ui/slide-line.tsx
+++ app/ui/slide-line.tsx
@@ -4,18 +4,21 @@ import Image from 'next/image';
 export default function SlideLine() {
   const products = [ ...clImage, ...clImage, ...clImage, ...clImage, ...clImage, ...clImage, ...clImage, ...clImage, ...clImage];
 
-  return products.map((image) => {
-
   return(
     <>
-    
-      <Image className="m-3 rounded-xl w-16 sm:w-52 h-14 sm:h-48 border-2 sm:border-4 rounded-xl border-x-fuchsia-500 border-y-fuchsia-500"
-        src={image.url}
-        width={200}
-        height={0}
-        alt={`${image.name} image`}
-      />
+
+      {products.map((image) => {
+        return (
+        <Image className="m-3 rounded-xl w-16 sm:w-52 h-14 sm:h-48 border-2 sm:border-4 rounded-xl border-x-fuchsia-500 border-y-fuchsia-500"
+          key={Math.random()}
+          src={image.url}
+          width={200}
+          height={0}
+          alt={`${image.name} image`}
+        />
+        );
+      })}
 
     </>
-  )})
+  );
 }
